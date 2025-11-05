2b0b84f3549984af621e554ad0ab9cbc75328d31
PapaSec <fransronald48@gmail.com>
2025-11-05 15:48:02 +0200

    Daily commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @livewireStyles
+</head>
+<body>
+    <i class="fa-thin fa-grid-2"></i>Home<br>
+    <i class="fab fa-laravel"></i>Lara
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return view('welcome');
+})->name('home');
 
 Route::get('/counter', function () {
     return response('Counter page');
