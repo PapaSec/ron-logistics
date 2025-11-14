50866cc77d41a9ae68787ee285d04c7ab4740a85
PapaSec <fransronald48@gmail.com>
2025-11-14 11:32:08 +0530

    Daily commit

--- app/Livewire/Shipments/Index.php
+++ app/Livewire/Shipments/Index.php
@@ -2,13 +2,17 @@
 
 namespace App\Livewire\Shipments;
 
-use Livewire\Component;
+use Livewire\{Component, WithPagination};
 use Livewire\Attributes\{Layout, Title};
 
 #[Layout('layouts.app')]
 #[Title('Dashboard - Ron Logistics')]
 class Index extends Component
 {
+    use WithPagination;
+
+    // Public Properties
+    
     public function render()
     {
         return view('livewire.shipments.index');
