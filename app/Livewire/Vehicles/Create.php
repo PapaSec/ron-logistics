9ef0a2eea97129d936e600d424e31fc9b8f04851
PapaSec <fransronald48@gmail.com>
2025-12-12 17:54:15 +0200

    Vehicle commit

--- app/Livewire/Vehicles/Create.php
+++ app/Livewire/Vehicles/Create.php
@@ -20,29 +20,6 @@ class Create extends Component
     public $notes = '';
 
     // VALIDATION RULES
-    protected $rules = [
-        'vehicle_number' => 'required|string|unique:vehicles,vehicle_number|max:50',
-        'type' => 'required|string|max:100',
-        'make' => 'required|string|max:100',
-        'model' => 'required|string|max:100',
-        'year' => 'required|integer|min:1900|max:' . (date('Y') + 1),
-        'license_plate' => 'required|string|unique:vehicles,license_plate|max:20',
-        'capacity' => 'required|numeric|min:0',
-        'status' => 'required|in:available,in_use,maintenance,out_of_service',
-        'last_maintenance' => 'nullable|date',
-        'next_maintenance' => 'nullable|date|after:last_maintenance',
-        'notes' => 'nullable|string',
-    ];
-
-    // CUSTOM VALIDATION MESSAGES
-    protected $messages = [
-        'vehicle_number.required' => 'Vehicle number is required',
-        'vehicle_number.unique' => 'This vehicle number is already taken',
-        'license_plate.required' => 'License plate is required',
-        'license_plate.unique' => 'This license plate is already taken',
-        'next_maintenance.after' => 'Next maintenance date must be after last maintenance date',
-    ];
-
     
 
     public function render()
