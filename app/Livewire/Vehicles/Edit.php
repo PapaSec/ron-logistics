44d92eb7090151f88268041c02236a205b0e6987
PapaSec <fransronald48@gmail.com>
2025-12-13 12:24:20 +0200

    Vehicle commit

--- app/Livewire/Vehicles/Edit.php
+++ app/Livewire/Vehicles/Edit.php
@@ -2,10 +2,28 @@
 
 namespace App\Livewire\Vehicles;
 
+use App\Models\Vehicle;
 use Livewire\Component;
 
 class Edit extends Component
 {
+    public Vehicle $vehicle;
+
+    // Form fields
+    public $vehicle_number = '';
+    public $type = '';
+    public $make = '';
+    public $model = '';
+    public $year = '';
+    public $license_plate = '';
+    public $capacity = '';
+    public $status = '';
+    public $notes = '';
+    public $last_maintenance = '';
+    public $next_maintenance = '';
+
+    
+
     public function render()
     {
         return view('livewire.vehicles.edit');
