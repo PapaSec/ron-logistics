9bd14a6a8b1f74bbf3800679221c26e1d21e44ee
PapaSec <fransronald48@gmail.com>
2025-12-13 12:29:38 +0200

    Vehicle commit

--- app/Livewire/Vehicles/Edit.php
+++ app/Livewire/Vehicles/Edit.php
@@ -22,7 +22,39 @@ class Edit extends Component
     public $last_maintenance = '';
     public $next_maintenance = '';
 
-    
+    // Common vehicle types for dropdown
+    public function getVehicleTypesProperty()
+    {
+        return [
+            'Truck' => 'Truck',
+            'Van' => 'Van',
+            'Pickup' => 'Pickup',
+            'Semi-Trailer' => 'Semi-Trailer',
+            'Box Truck' => 'Box Truck',
+            'Flatbed' => 'Flatbed',
+            'Refrigerated' => 'Refrigerated',
+            'Other' => 'Other',
+        ];
+    }
+
+    // Mount -  load vehicle data 
+    public function mount(Vehicle $vehicle)
+    {
+         $this->vehicle = $vehicle;
+        
+        // Populate form fields
+        $this->vehicle_number = $vehicle->vehicle_number;
+        $this->type = $vehicle->type;
+        $this->make = $vehicle->make;
+        $this->model = $vehicle->model;
+        $this->year = $vehicle->year;
+        $this->license_plate = $vehicle->license_plate;
+        $this->capacity = $vehicle->capacity;
+        $this->status = $vehicle->status;
+        $this->notes = $vehicle->notes;
+        $this->last_maintenance = $vehicle->last_maintenance?->format('Y-m-d');
+        $this->next_maintenance = $vehicle->next_maintenance?->format('Y-m-d');
+    }
 
     public function render()
     {
