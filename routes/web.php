692a53d7e1e2d5318255841074dbfb5385c2252d
PapaSec <fransronald48@gmail.com>
2025-12-18 14:53:04 +0200

    Daily commit

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Livewire\FuelMaintenance\{CreateFuel, CreateMaintenance, EditFuel, EditM
 use App\Livewire\Auth\{Login, Register};
 use App\Livewire\Dashboard\Index as DashboardIndex;
 use App\Livewire\DriverAssignments\Index as DriverAssignmentsIndex;
-use App\Livewire\Shipments\{Create as ShipmentsCreate, Edit as ShipmentsEdit, Index as ShipmentsIndex, Show as ShipmentsShow, Tracking as ShipmentsTracking};
+use App\Livewire\Shipments\{Create as ShipmentsCreate, Edit as ShipmentsEdit, Index as ShipmentsIndex, Show as ShipmentsShow};
 use App\Livewire\Drivers\{Create as DriversCreate, Edit as DriversEdit, Index as DriversIndex, Show as DriversShow};
 use App\Livewire\Vehicles\{Create as VehiclesCreate, Edit as VehiclesEdit, Index as VehiclesIndex, Show as VehiclesShow};
 
@@ -45,7 +45,6 @@ Route::middleware('auth')->group(function () {
         Route::get('/create', ShipmentsCreate::class)->name('create');
         Route::get('/{shipment}', ShipmentsShow::class)->name('show');
         Route::get('/{shipment}/edit', ShipmentsEdit::class)->name('edit');
-        Route::get('/tracking', ShipmentsTracking::class)->name('tracking');
     });
 
     // Drivers Management
