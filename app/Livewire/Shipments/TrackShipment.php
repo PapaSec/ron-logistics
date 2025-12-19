1d7bb011356dc60475f81c3b842c4a2fa71ad8fb
PapaSec <fransronald48@gmail.com>
2025-12-19 11:06:34 +0200

    TrackShipment commit

---
+++ app/Livewire/Shipments/TrackShipment.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Shipments;
+
+use Livewire\Component;
+
+class TrackShipment extends Component
+{
+    public function render()
+    {
+        return view('livewire.shipments.track-shipment');
+    }
+}
