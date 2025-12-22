cdf9817a569eb4cd407992c5ac46e3c442e28e21
PapaSec <fransronald48@gmail.com>
2025-12-22 19:19:07 +0200

    TrackShipment commit

--- app/Models/VehicleLocation.php
+++ app/Models/VehicleLocation.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class VehicleLocation extends Model
 {
-    //
+    protected $fillable = [
+        'vehicle_id',
+        'shipment_id',
+        'latitude',
+        'longitude',
+        'speed',
+        'heading',
+        'location_name',
+        'recorded_at',
+    ];
+
+    protected $casts = [
+        'latitude' => 'decimal:8',
+        'longitude' => 'decimal:8',
+        'speed' => 'decimal:2',
+        'recorded_at' => 'datetime',
+    ];
+    
 }
